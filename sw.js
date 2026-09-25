51b2f7f961748348ab9675ef2cc2a42d44e7199f
Wale Iwarere <iwasofficial@outlook.com>
2026-09-25 07:12:07 +0100

    Fix service worker asset list

--- sw.js
+++ sw.js
@@ -4,7 +4,9 @@ const ASSETS = [
   "./index.html",
   "./css/styles.css",
   "./js/storage.js",
-  "./js/app-core.js",\n  "./js/app-editor.js",\n  "./js/app-actions.js",
+  "./js/app-core.js",
+  "./js/app-editor.js",
+  "./js/app-actions.js",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
@@ -13,32 +15,36 @@ const ASSETS = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+
   event.respondWith(
     caches.match(event.request).then((cached) => {
-      const fetched = fetch(event.request)
-        .then((res) => {
-          if (res && res.ok) {
-            const copy = res.clone();
+      const network = fetch(event.request)
+        .then((response) => {
+          if (response && response.ok) {
+            const copy = response.clone();
             caches.open(CACHE).then((cache) => cache.put(event.request, copy));
           }
-          return res;
+          return response;
         })
         .catch(() => cached);
-      return cached || fetched;
+
+      return cached || network;
     })
   );
 });
